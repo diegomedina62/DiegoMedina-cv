7d355250fff136544d6dde702735a25a2fcfa98c
diegomedina62 <diegoleonmedina62@gmail.com>
2023-02-06 16:58:20 -0300

    add "dot" on experience section

--- src/components/experiencia/Rol.js
+++ src/components/experiencia/Rol.js
@@ -7,7 +7,24 @@ const Rol = ({ titulo, dataObject }) => {
         return (
           <div className="mx-8 mt-4 mb-10 flex flex-row space-x-4">
             {/* bisagra */}
-            <div className=" mt-4 w-1 bg-slate-600"></div>
+            <div className="relative mt-2 -mb-12 w-1 bg-slate-600 pt-1">
+              <div className="absolute -left-2 -top-1 h-fit w-fit">
+                <svg
+                  className="stroke-slate-600"
+                  viewBox="0 0 4 4"
+                  width="1.25rem"
+                  height="1.25rem"
+                >
+                  <circle
+                    cx="2"
+                    cy="2"
+                    r="1.5"
+                    stroke-width="0.7"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </div>
 
             {/* container de "lugar trabajados" */}
             <div className="w-full pl-2">
