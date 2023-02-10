a0e1c15980e88d37e1b71545dd297597eb5a78a8
diegomedina62 <diegoleonmedina62@gmail.com>
2023-02-10 12:22:15 -0300

    logos grid implementation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,19 +12,24 @@ const Skills = () => {
     <section id="skills">
       <h1 className="">Habilidades</h1>
       {/* celdas de skills  */}
-      <div className=" my-4 flex flex-col space-y-6">
+      <div className=" flex- my-4 flex-col space-y-6 md:mx-6 md:grid md:grid-cols-3 md:gap-6 md:space-y-0 ">
+        {/* Celdas de habilidades */}
         {skillsAbout.map((x) => {
           return (
-            <div key={x.id}>
-              <div className=" h-full w-full rounded-lg bg-slate-100 p-2 shadow-lg">
-                <h3 className="font-semibold">{x.skill}</h3>
-                <p className="pl-2 pt-2 text-sm">{x.texto}</p>
-              </div>
+            <div
+              key={x.id}
+              className="  h-full w-full rounded-lg bg-slate-50 p-2 shadow-lg "
+            >
+              <h3 className="font-semibold">{x.skill}</h3>
+              <p className="pl-2 pt-2 text-sm">{x.texto}</p>
             </div>
           );
         })}
-        <div className="h-full w-full rounded-lg bg-slate-100 p-2 shadow-lg">
-          <h3 className="fontd-semibold">Heramientas de programación</h3>
+        {/* Celda de logos */}
+        <div className=" h-full w-full rounded-lg bg-slate-50 p-2 shadow-lg md:col-start-2 md:col-end-4 md:row-span-4 md:row-start-1">
+          <h3 className="fontd-semibold">
+            Programar con las siguientes herramientas
+          </h3>
           <Logos titulo="Web development" logosObject={logosWeb} />
           <Logos titulo="Data Managment" logosObject={logosData} />
           <Logos titulo="Base de Datos" logosObject={logosDatabase} />
